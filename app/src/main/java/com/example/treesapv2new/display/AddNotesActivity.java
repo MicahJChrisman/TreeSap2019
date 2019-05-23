7e8b8bf716dd344487ace81265871050765aa44a
PIPPIN\micahchrisman <micah.chrisman@hope.edu>
2019-05-23 16:43:48 -0400

    no message

--- app/src/main/java/com/example/treesapv2new/display/AddNotesActivity.java
+++ app/src/main/java/com/example/treesapv2new/display/AddNotesActivity.java
@@ -1,11 +1,15 @@
 package com.example.treesapv2new.display;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 import com.example.treesapv2new.R;
+import com.example.treesapv2new.Tree_Info_First;
 
 public class AddNotesActivity extends AppCompatActivity {
     @Override
@@ -13,9 +17,17 @@ public class AddNotesActivity extends AppCompatActivity {
         super.onCreate(null);
         setContentView(R.layout.add_notes_display);
 
-
+        Button button = (Button) findViewById(R.id.add_notes_button);
+        button.setOnClickListener(new AddNotesEvent());
     }
 
-
+    private class AddNotesEvent implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+//            Intent intentA = new Intent(AddNotesActivity.this, AddNotesActivity.class);
+//            startActivity(intentA);
+            finish();
+        }
+    }
 
 }
