884fd62011cb27ba479a1c848b9945544c6290c1
PIPPIN\micahchrisman <micah.chrisman@hope.edu>
2019-05-31 13:38:28 -0400

    Changes to the add tree page

--- app/src/main/java/com/example/treesapv2new/Add_Tree_Activity.java
+++ app/src/main/java/com/example/treesapv2new/Add_Tree_Activity.java
@@ -1,18 +1,116 @@
 package com.example.treesapv2new;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Add_Tree_Activity extends AppCompatActivity {
+import java.util.List;
+
+public class Add_Tree_Activity extends AppCompatActivity implements LocationListener {
+    Double longitude, latitude;
+    LocationManager locationManager;
+    final long LOCATION_REFRESH_TIME = 1;     // 1 minute
+    final long LOCATION_REFRESH_DISTANCE = 1; // 10 meters
+    private static final String[] PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_NETWORK_STATE
+    };
+    private static final int REQUEST_ID = 6;
+
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(null);
         setContentView(R.layout.add_tree_page);
 
         Button b = (Button) findViewById(R.id.next_add_tree);
         b.setOnClickListener(new NextEvent());
+
+        Button getGPSbutton = (Button) findViewById(R.id.get_location_button);
+        getGPSbutton.setOnClickListener(new getGpsEvent());
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getBaseContext(), "Permissions are not right", Toast.LENGTH_SHORT).show();
+        }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, this);
+    }
+
+    public void getLocation(){
+        Location location = null;
+        boolean isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGPSEnabled) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(PERMS, REQUEST_ID);
+                }
+                return;
+            } else {
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Criteria criteria = new Criteria();
+                Location defaultLocation = new Location("");
+                defaultLocation.setLatitude(42.788002);
+                defaultLocation.setLongitude(-86.105971);
+                List<String> provs = locationManager.getAllProviders();
+                for(String prov : provs) {
+                    if (locationManager.getLastKnownLocation(prov) != null) {
+                        if (!prov.equals("gps")) {
+                            defaultLocation = locationManager.getLastKnownLocation(prov);
+                        } else {
+                            location = locationManager.getLastKnownLocation(prov);
+                        }
+                        if (location != null) {
+                            break;
+                        }
+                    }
+                }
+                if(location==null){
+                    location = defaultLocation;
+                }
+            }
+
+            onLocationChanged(location);
+
+
+        } else {
+            new android.app.AlertDialog.Builder(this)
+                    .setTitle("Location Services are not enabled")
+                    .setMessage("The location services on your phone are not enabled.  Please turn on GPS.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        }
+                    }).show();
+        }
+    }
+
+    private class getGpsEvent implements View.OnClickListener{
+        public void onClick(View v){
+            getLocation();
+            TextView latPut = (TextView) findViewById(R.id.lat_putter);
+            latPut.setText(Double.toString(latitude));
+            TextView longPut = (TextView) findViewById(R.id.long_putter);
+            longPut.setText(Double.toString(longitude));
+            findViewById(R.id.lat_layout).setVisibility(View.VISIBLE);
+            findViewById(R.id.long_layout).setVisibility(View.VISIBLE);
+            findViewById(R.id.next_add_tree).setVisibility(View.VISIBLE);
+        }
     }
 
 
@@ -22,5 +120,23 @@ public class Add_Tree_Activity extends AppCompatActivity {
             Intent intentA = new Intent(Add_Tree_Activity.this, Tree_Bark_Activity.class);
             startActivity(intentA);
         }
+    }
+
+    public void onLocationChanged(Location location) {
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
