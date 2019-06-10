bd8e175f3a1196d796f51fe3edec25a9f72d33aa
PIPPIN\micahchrisman <micah.chrisman@hope.edu>
2019-06-10 16:56:08 -0400

    changing pie chart display agaiin

--- app/src/main/java/com/example/treesapv2new/Pie_Chart_Activity.java
+++ app/src/main/java/com/example/treesapv2new/Pie_Chart_Activity.java
@@ -427,6 +427,7 @@ public class Pie_Chart_Activity extends AppCompatActivity {
         pieChart.invalidate();
         pieChart.setCenterText(commonName +" Annual Benefits equals "+ entry);
         pieChart.setCenterTextSize(20);
+        pieChart.setHoleColor(Color.TRANSPARENT);
         Description description = new Description();
         description.setText("");
         pieChart.setDescription(description);
