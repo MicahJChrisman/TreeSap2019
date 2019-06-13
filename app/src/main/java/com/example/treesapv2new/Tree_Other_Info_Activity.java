382b397c3402bad67689301f1c2f9af2988327d2
PIPPIN\micahchrisman <micah.chrisman@hope.edu>
2019-06-13 10:53:24 -0400

    Adding trees now adds to the server

--- app/src/main/java/com/example/treesapv2new/Tree_Other_Info_Activity.java
+++ app/src/main/java/com/example/treesapv2new/Tree_Other_Info_Activity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -16,13 +18,37 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.treesapv2new.datasource.UserTreeDataSource;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Tree_Other_Info_Activity extends AppCompatActivity {
     String[] passedArray = new String[10];
 
-
+    String commonName;
+    String scientificName;
+    String dbh;
+    String lat;
+    String longit;
+    String barkPic;
+    String leafPic;
+    String fullPic;
+    String notes;
+    String userID;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(null);
@@ -49,23 +75,27 @@ public class Tree_Other_Info_Activity extends AppCompatActivity {
                 Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
                 ImageView image = (ImageView) findViewById(R.id.show_bark_pic);
                 //image.setImageBitmap(bmp);
-                passedArray[7] = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                barkPic = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                passedArray[7] = barkPic;
             }
             byte[] byteArrayLeaf = extras.getByteArray("leaf_pic_byte_array");
             if(byteArrayLeaf != null) {
                 Bitmap bmp = BitmapFactory.decodeByteArray(byteArrayLeaf, 0, byteArrayLeaf.length);
                 ImageView image = (ImageView) findViewById(R.id.show_leaf_pic);
                 //image.setImageBitmap(bmp);
-                passedArray[8] = Base64.encodeToString(byteArrayLeaf, Base64.DEFAULT);
+                leafPic = Base64.encodeToString(byteArrayLeaf, Base64.DEFAULT);
+                passedArray[8] = leafPic;
             }
             byte[] byteArrayFull = extras.getByteArray("full_pic_byte_array");
             if(byteArrayFull != null) {
                 Bitmap bmp = BitmapFactory.decodeByteArray(byteArrayFull, 0, byteArrayFull.length);
                 ImageView image = (ImageView) findViewById(R.id.show_tree_pic);
                 //image.setImageBitmap(bmp);
-                passedArray[9] = Base64.encodeToString(byteArrayFull, Base64.DEFAULT);
+                fullPic = Base64.encodeToString(byteArrayFull, Base64.DEFAULT);
+                passedArray[9] = fullPic;
             }
-
+            lat = lat_value;
+            longit = long_value;
             passedArray[4] = lat_value;
             passedArray[5] = long_value;
         }
@@ -142,18 +172,50 @@ public class Tree_Other_Info_Activity extends AppCompatActivity {
             builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+                    commonName = ((AutoCompleteTextView) findViewById(R.id.common_name)).getText().toString();
+                    scientificName = ((TextView) findViewById(R.id.scientific_name)).getText().toString();
+                    dbh = ((TextView) findViewById(R.id.dbh_edit)).getText().toString();
+                    notes = ((TextView) findViewById(R.id.notes_about_tree)).getText().toString();
+
+
+
                     passedArray[0] = "0";
-                    passedArray[1] = ((AutoCompleteTextView) findViewById(R.id.common_name)).getText().toString();
-                    passedArray[2] = ((TextView) findViewById(R.id.scientific_name)).getText().toString();
+                    passedArray[1] = commonName;
+                    passedArray[2] = scientificName;
                     if(((Switch) findViewById(R.id.units_switch)).isChecked()) {
-                        Double t = Double.valueOf(((TextView) findViewById(R.id.dbh_edit)).getText().toString());
+                        Double t = Double.valueOf(dbh);
                         t=t*0.393701;
 
                         passedArray[3] = t.toString();
                     }else{
-                        passedArray[3] = ((TextView) findViewById(R.id.dbh_edit)).getText().toString();
+                        passedArray[3] = dbh;
                     }
-                    passedArray[6] = ((TextView) findViewById(R.id.notes_about_tree)).getText().toString();
+                    passedArray[6] = notes;
+
+
+                    //Send new tree to the database
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+                    CollectionReference users = db.collection("Pending Trees");
+
+                    userID = firebaseUser.getUid();
+
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Common Name",commonName);
+                    user.put("Scientific Name", scientificName);
+                    user.put("Latitude", lat);
+                    user.put("Longitude",longit);
+                    user.put("DBH", dbh);
+                    user.put("Other Info", notes);
+                    ArrayList<String> picArray = new ArrayList<String>();
+                    picArray.add(barkPic);
+                    picArray.add(leafPic);
+                    picArray.add(fullPic);
+                    user.put("Pictures", picArray);
+                    user.put("UserID", userID);
+                    users.document(lat + ", " + longit + ", " + commonName).set(user);
+
+
                     dialog.dismiss();
                     MainActivity.userTreeDataSourceGlobal.initialize(Tree_Other_Info_Activity.this,null);
                     MainActivity.userTreeDataSourceGlobal.addTree(passedArray);
